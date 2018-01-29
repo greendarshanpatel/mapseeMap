32bf038848d01f3a59fac05e8af0b82b2a3ddd76
Darshan Patel <darsh1538@gmail.com>
2018-01-29 22:12:15 +0530

    api Key added tested

--- mapseeMap/AppDelegate.swift
+++ mapseeMap/AppDelegate.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+   // AIzaSyAtP7JSyq5q_ZFU7yewYd0hh-YqTXWESS0
 
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAtP7JSyq5q_ZFU7yewYd0hh-YqTXWESS0")
         return true
     }
 
@@ -40,6 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
 
 
 }
